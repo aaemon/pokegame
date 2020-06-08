dc7d366fe3119605bbfce9b253da9717206f1e61
Ammer Ashraf Emon <aaemon@ip-172-31-74-22.ec2.internal>
2020-06-08 03:30:06 +0000

    Updates

---
+++ precache-manifest.ae26d5876ef7fbcdd0eac7daa0e4ef82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2729778a4897bb4256176b8983d5e8c0",
+    "url": "/pokegame/index.html"
+  },
+  {
+    "revision": "c68bd4009c308bb06323",
+    "url": "/pokegame/static/css/main.571aa498.chunk.css"
+  },
+  {
+    "revision": "5fc6ccd4ce7cc3fecc7a",
+    "url": "/pokegame/static/js/2.30bd0e30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokegame/static/js/2.30bd0e30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c68bd4009c308bb06323",
+    "url": "/pokegame/static/js/main.a478b85d.chunk.js"
+  },
+  {
+    "revision": "6d1106e5eda2ef9a3928",
+    "url": "/pokegame/static/js/runtime-main.579f8ae2.js"
+  }
+]);
